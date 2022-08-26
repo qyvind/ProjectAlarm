093959bcc3753045ed79419a5beb8ba8a23c8993
qf <qf@framo.no>
2022-08-26 13:11:59 +0200

    KOMMENTAR LAGT TIL

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pymssql
 import time
+#import anvil.server sdrfgserfg
 
+#anvil.server.connect('server_IPPX6HHMIQPDD5DMHBWN5AL5-XSOZPU4RPRPRIKK5')
+
+# ny kommentar
+print('dette er master')
 
 if __name__ == '__main__':
     conn = pymssql.connect('fm-sql05', 'mf_byod_reader', 'lOBCeVgYOC1hDXsVrNzn', 'byod_staging')
     cursor = conn.cursor(as_dict=True)
 
     print('Sjekk prosjektnummer:')
+
     project = input()
 
     found = False
@@ -17,6 +23,6 @@ if __name__ == '__main__':
         print(f"Ser etter prosjekt {project}")
         if rows > 0:
             found = True
-            print(f"************** Prosjekt {project} er kommet over i byod ****************")
+            print(f"***** Prosjekt {project} er kommet over i byod *******")
         else:
             time.sleep(5)
